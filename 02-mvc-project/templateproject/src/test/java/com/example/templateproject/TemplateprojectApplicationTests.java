e41ca37bdf653b1df78f5b181ad85588c60e1dd8
R1NN <r1nn650t@gmail.com>
2026-04-07 13:34:49 +0900

    Create: 02-mvc-project/templateproject/

---
+++ 02-mvc-project/templateproject/src/test/java/com/example/templateproject/TemplateprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.templateproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
