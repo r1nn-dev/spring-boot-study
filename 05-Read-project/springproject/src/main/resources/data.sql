d3d2e3fd4162c8b30be4b21d5f67c83c349b95dd
R1NN <r1nn650t@gmail.com>
2026-04-14 14:11:38 +0900

    Create: 05-Read-project/springproject

---
+++ 05-Read-project/springproject/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO article (title, content) VALUES
+    ('Spring Boot 소개', 'Spring Boot는 자바 기반의 웹 프레임워크이다.'),
+    ('JPA란?', 'JPA는 Java Persistence API의 약자로 ORM 기술이다.'),
+    ('REST API란?', 'REST API는 웹에서 자원을 CRUD 하는 방식이다.'),
+    ('MySQL 사용법', 'MySQL은 대표적인 오픈소스 관계형 데이터베이스이다.'),
+    ('Gradle이란?', 'Gradle는 빌드 자동화 도구로, Spring Boot에서 많이 사용된다.');
