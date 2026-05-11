e28d5bc69894861c86bbd1e7f83e3513cf09cca8
R1NN <r1nn650t@gmail.com>
2026-05-12 03:39:34 +0900

    Update: 08-Delete-project/memberproject

---
+++ 08-Delete-project/memberproject/src/main/java/com/example/memberproject/MemberprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.memberproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberprojectApplication.class, args);
+	}
+
+}
