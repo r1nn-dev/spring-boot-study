88227fd1e7cb2a12be8591e1dfcdba91d862d43c
R1NN <r1nn650t@gmail.com>
2026-05-26 02:07:49 +0900

    Update: 11-stream-project/springproject/.../StreamEx2.java

--- 11-stream-project/springproject/src/main/java/com/example/springproject/edu/StreamEx2.java
+++ 11-stream-project/springproject/src/main/java/com/example/springproject/edu/StreamEx2.java
@@ -14,9 +14,13 @@ public class StreamEx2 {
         List<Integer> integerList = Arrays.asList(1, 2, 3, 1, 2, 3, 4, 5, 7, 7, 8, 9, 10, 10);
 
         // 1. forEach문 사용
+
+        // ArrayList 생성
+        // HashSet - 중복 제거
         List<Integer> uniqueNumber = new ArrayList<>(new HashSet<>(integerList));
         List<Integer> filteredList = new ArrayList<>();
 
+        // 3의 배수가 아닌 수 필터링
         for (Integer num : uniqueNumber) {
             if (num % 3 != 0) filteredList.add(num);
         }
@@ -24,10 +28,13 @@ public class StreamEx2 {
         System.out.println(filteredList);
 
         // 2. Stream 사용
+        // 2-1) Stream 생성
+        // 2-2) 중간 연산 - 중복 제거, 3의 배수가 아닌 수
+        // 2-3) 최종 연산 - Stream -> List로 type 변환 후 수집
         System.out.println(
-                integerList.stream().distinct()               // 중복 제거 -> {1, 2, 3, 4, 5, 7, 8, 9, 10}
-                        .filter(num -> num % 3 != 0)   // 3의 배수가 아닌 수 -> {1, 2, 4, 5, 7, 8, 10}
-                        .collect(Collectors.toList())         // Stream -> List로 type 변환 후 수집 -> {1, 2, 4, 5, 7, 8, 10}
+                integerList.stream().distinct()               // 중복 제거 -> (1, 2, 3, 4, 5, 7, 8, 9, 10)
+                        .filter(num -> num % 3 != 0)   // 3의 배수가 아닌 수 -> (1, 2, 4, 5, 7, 8, 10)
+                        .collect(Collectors.toList())         // List로 변환 -> [1, 2, 4, 5, 7, 8, 10]
         );
     }
 }
