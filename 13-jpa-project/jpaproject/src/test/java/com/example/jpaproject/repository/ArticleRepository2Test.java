6929a7c649f8ff3d9450b6cfc9cc406cf0967e81
R1NN <r1nn650t@gmail.com>
2026-05-26 14:21:52 +0900

    Update: 13-jpa-project/jpaproject/.../test/

--- 13-jpa-project/jpaproject/src/test/java/com/example/jpaproject/repository/ArticleRepository2Test.java
+++ 13-jpa-project/jpaproject/src/test/java/com/example/jpaproject/repository/ArticleRepository2Test.java
@@ -1,4 +1,52 @@
 package com.example.jpaproject.repository;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import java.util.List;
+
+@DataJpaTest
 public class ArticleRepository2Test {
+
+    @Autowired
+    private ArticleRepository2 articleRepository;
+
+    //title이 "keyword"인 article 리스트 가져오기
+    @Test
+    public void findByTitleTest(){
+        articleRepository.findByTitle("Entity란?")
+                .forEach(System.out::println);
+    }
+
+    //타이틀이 "keyword"를 포함하고 있는 리스트의 갯수 가져오기
+    @Test
+    public void findByTitleContainingTest() {
+        articleRepository.findByTitleContaining("Spring")
+                .forEach(System.out::println);
+    }
+
+    //content가 "keyword"을 포함하고 있는 article 리스트 가져오기
+    @Test
+    public void findByContentContainingTest(){
+        articleRepository.findByContentContaining("JPA")
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void findByTitle2(){
+        articleRepository.findByTitle2("JPA")
+                .forEach(System.out::println);
+    }
+    @Test
+    public void findArticlesInRange(){
+        articleRepository.findArticlesInRange(2L, 5L)
+                .forEach(System.out::println);
+    }
+    @Test
+    public void findByIdsInTest(){
+        articleRepository.findByIdsIn(List.of(1L, 2L, 5L))
+                .forEach(System.out::println);
+    }
+
 }
--- 13-jpa-project/jpaproject/src/test/java/com/example/jpaproject/repository/ArticleRepositoryTest.java
+++ 13-jpa-project/jpaproject/src/test/java/com/example/jpaproject/repository/ArticleRepositoryTest.java
@@ -1,4 +1,34 @@
 package com.example.jpaproject.repository;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.data.domain.Sort;
+
+@DataJpaTest
 public class ArticleRepositoryTest {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Test
+    void contextLoad() {
+        //id로 검색
+        articleRepository.findById(2L).ifPresentOrElse(
+                System.out::println,
+                () -> System.out.println("Article Not Found")
+        );
+
+        //Article List 가져오기
+        articleRepository.findAll().forEach(System.out::println);
+
+        //Article List 가져오기(id 역순으로 정렬)
+        articleRepository.findAll(Sort.by(Sort.Direction.DESC, "id"))
+                .forEach(System.out::println);
+
+        //Article 갯수 출력
+        long count = articleRepository.count();
+        System.out.println(count);
+    }
+
 }
