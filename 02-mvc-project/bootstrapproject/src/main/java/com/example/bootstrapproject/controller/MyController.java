f5c98ff3ef455dbe17bc04162331689fb0ab2456
R1NN <r1nn650t@gmail.com>
2026-06-06 23:55:41 +0900

    Update: 02-mvc-project/bootstrapproject/.../MyController.java

--- 02-mvc-project/bootstrapproject/src/main/java/com/example/bootstrapproject/controller/MyController.java
+++ 02-mvc-project/bootstrapproject/src/main/java/com/example/bootstrapproject/controller/MyController.java
@@ -8,20 +8,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MyController {
 
     @RequestMapping("/test")            // "/test"로 들어오는 GET 요청을 이 메서드가 처리한다.
-    public String test(Model model) {   // Model 객체를 통해 View에 전달할 데이터를 담는다.
+    public String test(Model model) {
+        // View 데이터를 Model에 저장한다.
         model.addAttribute("username", "이지은");    // "username"이라는 이름으로 데이터를 저장한다.
-        return "greeting";                  // greetings.mustache 파일을 찾아 렌더링한다.
+        // greetings.mustache 파일을 찾아 렌더링한다.
+        return "greeting";
     }
 
     @RequestMapping("/hi")             // "/hi"로 들어오는 GET 요청을 이 메서드가 처리한다.
-    public String hi(Model model) {   // Model 객체를 통해 View에 전달할 데이터를 담는다.
+    public String hi(Model model) {
+        // View 데이터를 Model에 저장한다.
         model.addAttribute("username", "이지은");    // "username"이라는 이름으로 데이터를 저장한다.
-        return "greetings";                  // greetings.mustache 파일을 찾아 렌더링한다.
+        // greetings.mustache 파일을 찾아 렌더링한다.
+        return "greetings";
     }
     @RequestMapping("/bye")             // "/bye"로 들어오는 GET 요청을 이 메서드가 처리한다.
-    public String bye(Model model) {   // Model 객체를 통해 View에 전달할 데이터를 담는다.
+    public String bye(Model model) {
+        // View 데이터를 Model에 저장한다.
         model.addAttribute("nickname", "아이유");    // "nickname"이라는 이름으로 데이터를 저장한다.
-        return "goodbye";                 // goodbye.mustache 파일을 찾아 렌더링한다.
+        // goodbye.mustache 파일을 찾아 렌더링한다.
+        return "goodbye";
     }
 
 }
